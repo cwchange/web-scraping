1ad7673434985bcbc162059c97a081895b8ac4db
cwchange <cwchange@gmail.com>
2015-08-03 08:46:48 -0400

    Create example_02.py

---
+++ example_02.py
@@ -0,0 +1,24 @@
+import urllib, urllib2, cookielib
+ 
+‪#‎create‬ a cookie file to store the cookie
+
+cj = cookielib.CookieJar()
+opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+urllib2.install_opener(opener)
+ 
+‪#‎pass‬ the login information to the opener
+
+login_data = urllib.urlencode({’username’ : your_username, ‘password’ : your_password,’submit’ : ‘Submit’, ‘id’: ‘U45i83’ })
+ 
+‪#‎replace‬ the “the_login_url” to the url that require login, example:http://example.com/login.php
+
+resp = opener.open(“the_login_url”, login_data)
+#resp= opener.open("www.example.com/lgoin.php", login_data)
+ 
+‪#‎You‬ can print the output, the output will be in HTML
+
+print resp.read()
+ 
+‪#‎going‬ to other url which share the same cookie
+
+data=opener.open(“another url”)
